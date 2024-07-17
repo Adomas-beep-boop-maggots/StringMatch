8c60215e2b8418d10fd2e5fdcfc46239cabb3759
Adomas Daškevičiaus <adomasda@gmail.com>
2024-07-17 11:28:33 +0300

    String_searcher first commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,62 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+    static String FindMatchInString_1(String l, String Match) {
+        char c;
+        int MatchLength = Match.length();
+        int NeedToFind = MatchLength;
+        for (int i = 0; i <= l.length() - MatchLength; i++) {
+            c = l.charAt(i);
+            while ((c == Match.charAt(MatchLength - NeedToFind))) {
+                NeedToFind--;
+                if (NeedToFind == 0) {
+                    return l;
+                }
+                i++;
+                c = l.charAt(i);
+            }
+            NeedToFind = MatchLength;
         }
+        return null;
+    }
+
+    static String FindMatchInString_2(String l, String Match) {
+        if (l.contains(Match)) {
+            return l;
+        } else {
+            return null;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        if (args.length == 0) {
+            System.err.println("Provide at 1 argument.");
+            System.err.println("Usage: StringMatch [String]");
+            System.exit(1);
+        }
+        String FindThis = args[0];
+        BufferedReader inputStream = new BufferedReader(new FileReader("dataset"));
+        String l;
+        String f;
+        try {
+            while ((l = inputStream.readLine()) != null) {
+
+                // FindMatchInString implementations:
+                // FindMatchInString_1
+                // FindMatchInString_2
+                if ((f = FindMatchInString_1(l, FindThis)) != null) {
+                    System.out.println("Found it here " + f);
+                    System.exit(0);
+                }
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+        System.err.println("NOT found");
+        System.exit(1);
     }
 }
