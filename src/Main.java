c78357dc658ceaaa9a29d77eda0361cda8c3e7a6
Adomas Daškevičiaus <adomasda@gmail.com>
2024-07-17 13:18:19 +0300

    removed useless function & comment

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 
 public class Main {
-    static String findMatchInString1(String l, String match) {
-        char c;
-        int matchLength = match.length();
-        int needToFind = matchLength;
-        for (int i = 0; i <= l.length() - matchLength; i++) {
-            c = l.charAt(i);
-            while ((c == match.charAt(matchLength - needToFind))) {
-                needToFind--;
-                if (needToFind == 0) {
-                    return l;
-                }
-                i++;
-                c = l.charAt(i);
-            }
-            needToFind = matchLength;
-        }
-        return null;
-    }
-
-    static String findMatchInString2(String l, String Match) {
+    static String findMatchInString(String l, String Match) {
         if (l.contains(Match)) {
             return l;
         } else {
@@ -42,11 +23,7 @@ public class Main {
         String findLine;
         try {
             while ((l = inputStream.readLine()) != null) {
-
-                // FindMatchInString implementations:
-                // findMatchInString1
-                // findMatchInString2
-                if ((findLine = findMatchInString1(l, findThis)) != null) {
+                if ((findLine = findMatchInString(l, findThis)) != null) {
                     System.out.println("Found it here " + findLine);
                     System.exit(0);
                 }
